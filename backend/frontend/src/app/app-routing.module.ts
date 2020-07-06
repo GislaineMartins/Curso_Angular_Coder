00fe389c5ac45590939a71230ed03b2f8bc1ae77
Gislaine <gimcjf@gmail.com>
2020-07-06 14:37:59 -0300

    alterações de arquivos e criação de componentes

--- backend/frontend/src/app/app-routing.module.ts
+++ backend/frontend/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule, ModuleWithProviders } from '@angular/core'
 import { Routes, RouterModule } from '@angular/router'
 import { HomeComponent } from './views/home/home.component'
 import { ProductCrudComponent } from './views/product-crud/product-crud.component'
+import {ProductCreateComponent} from './components/product/product-create/product-create.component'
 
 
 
  const API_ROUTES: Routes = [
     { path: '', component: HomeComponent},
-    {path: 'products', component: ProductCrudComponent}
+    {path: 'products', component: ProductCrudComponent},
+    {path: 'products/create', component: ProductCreateComponent}
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(API_ROUTES);
--- backend/frontend/src/app/app.module.ts
+++ backend/frontend/src/app/app.module.ts
@@ -14,6 +14,8 @@ import { HomeComponent } from './views/home/home.component'
 import {MatCardModule} from '@angular/material/card';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { RedDirective } from './directives/red.directive';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
+import {MatButtonModule} from '@angular/material/button'
 
 
 
@@ -26,6 +28,7 @@ import { RedDirective } from './directives/red.directive';
     HomeComponent,
     ProductCrudComponent,
     RedDirective,
+    ProductCreateComponent,
      
 
   ],
@@ -36,6 +39,7 @@ import { RedDirective } from './directives/red.directive';
     MatSidenavModule,
     MatListModule,
     MatCardModule,
+    MatButtonModule,
     routing
     
   ],
