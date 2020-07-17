c57a054229da670347d081bcd1c77887f3ccea8e
Gislaine <gimcjf@gmail.com>
2020-07-16 22:26:11 -0300

    alterações em arquivos

--- backend/frontend/src/app/app.module.ts
+++ backend/frontend/src/app/app.module.ts
@@ -21,7 +21,11 @@ import {HttpClientModule} from '@angular/common/http'
 import {FormsModule} from '@angular/forms'
 import {MatFormFieldModule} from '@angular/material/form-field'
 import {MatInputModule} from '@angular/material/input';
-import { ProductReadComponent } from './components/product/product-read/product-read.component'
+import { ProductReadComponent } from './components/product/product-read/product-read.component';
+import { ProductRead2Component } from './components/product/product-read2/product-read2.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort'
 
 
 
@@ -36,6 +40,7 @@ import { ProductReadComponent } from './components/product/product-read/product-
     RedDirective,
     ProductCreateComponent,
     ProductReadComponent,
+    ProductRead2Component,
      
 
   ],
@@ -52,7 +57,10 @@ import { ProductReadComponent } from './components/product/product-read/product-
     FormsModule,
     MatFormFieldModule,
     MatInputModule,
-    routing
+    routing,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
     
   ],
   providers: [],
