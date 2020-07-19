982350bc54cfeb59fd1261d3262e2392b74d90ce
Gislaine <gimcjf@gmail.com>
2020-07-18 21:13:36 -0300

    alterações

--- backend/frontend/src/app/app.module.ts
+++ backend/frontend/src/app/app.module.ts
@@ -27,7 +27,8 @@ import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort'
 import localept from '@angular/common/locales/pt'
-import {registerLocaleData} from '@angular/common'
+import {registerLocaleData} from '@angular/common';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component'
 
 registerLocaleData(localept)
 
@@ -45,6 +46,7 @@ registerLocaleData(localept)
     ProductCreateComponent,
     ProductReadComponent,
     ProductRead2Component,
+    ProductUpdateComponent,
      
 
   ],
